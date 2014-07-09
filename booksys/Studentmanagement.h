7de7315486bb09ebbc1e905cb34737003f8d3d0e
wuyongxing <350193001@qq.com>
2014-07-09 20:40:19 +0800

    modify show find next

--- booksys/Studentmanagement.h
+++ booksys/Studentmanagement.h
@@ -5,12 +5,15 @@ using namespace std;
 class Studentmanagement
 {
 public:
+	static fstream file;
+	static string findname;
     static bool check1(string ID2);
     void add(int n);
     static void add1(string ID1,string password1,string name1);
-    void show();
-    void finds(string name2);
-    void finds(long long id2);
+    static void finds(string name2);
+	static Student show(bool &x);
+	static Student nextname(bool &y);
+	static Student nextid(bool &z);
     void modify(string name2,string sex2,int age2);
     void delet(string name2,string sex2,int age2);
     bool comp(string a,string b);
