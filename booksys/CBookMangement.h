b268828cfe3e215b5796b39adb60a42df57d96bb
JJWSSS <1012109715@qq.com>
2014-07-09 22:04:32 +0800

    Íê³ÉÁ˽軹ÊéɾÊé

--- booksys/CBookMangement.h
+++ booksys/CBookMangement.h
@@ -11,7 +11,10 @@ class CBookMangement
 	CBook book;*/
 private:
 	void AddBookInformation(string a, long b, int c, int d, int x, int y, int z);
-	CBook BookDateSearch(int year, int month, int day);
-	CBook BookNameSearch(string name);
-	CBook BookIDSearch(long a);
+	CBook BookDateSearch(bool &end,int year, int month, int day);
+	CBook BookNameSearch(bool &end,string name);
+	CBook BookIDSearch(bool &end,long a);
+	void BookReturn(bool &end, long ID);
+	void BookDelete(bool &end, long ID);
+	void BookSent(bool &end, long ID);
 };
